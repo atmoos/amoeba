5591f2b9081f4854a4d635fe0b6700ed68c92af8
Thomas Kägi <kaegit@gmail.com>
2020-12-21 16:57:33 +0100

    Marginally improve performance of naive search.

--- patternMatching/Naive/MultiPatternSearch.cs
+++ patternMatching/Naive/MultiPatternSearch.cs
@@ -5,58 +5,96 @@ using System.Collections.Generic;
 namespace patternMatching.Naive
 {
     public sealed class MultiPatternSearch<TAlphabet, TMatch> : ISearchBuilder<TAlphabet, TMatch>
+        where TAlphabet : IEquatable<TAlphabet>
     {
         private readonly List<(IEnumerable<TAlphabet>, TMatch)> dictionary = new List<(IEnumerable<TAlphabet>, TMatch)>();
         public void Add(IEnumerable<TAlphabet> pattern, in TMatch match) => this.dictionary.Add((pattern, match));
-
-        public ISearch<TAlphabet, TMatch> Build() => new NaiveSearch(GuardForKeyCollisions(this.dictionary.Select(Build)));
-        private static (List<TAlphabet>, TMatch match) Build((IEnumerable<TAlphabet> p, TMatch m) value) => (value.p.ToList(), value.m);
-        private static IEnumerable<(List<TAlphabet> pattern, TMatch match)> GuardForKeyCollisions(IEnumerable<(List<TAlphabet> pattern, TMatch match)> dictionary)
+        public ISearch<TAlphabet, TMatch> Build() => new NaiveSearch(GuardForKeyCollisions(this.dictionary));
+        private static List<(Scan pattern, TMatch match)> GuardForKeyCollisions(IEnumerable<(IEnumerable<TAlphabet> pattern, TMatch match)> dictionary)
         {
-            var keyCollisionGuard = new List<List<TAlphabet>>();
-            foreach(var kv in dictionary) {
+            var distinctKeys = new List<(Scan pattern, TMatch match)>();
+            foreach(var (pattern, match) in dictionary) {
                 var keyIsUnique = true;
-                foreach(var guard in keyCollisionGuard) {
-                    if(guard.SequenceEqual(kv.pattern)) {
+                var scan = new Scan(pattern.ToArray());
+                foreach(var (guard, _) in distinctKeys) {
+                    if(guard.Equal(scan)) {
                         keyIsUnique = false;
                         break;
                     }
                 }
                 if(keyIsUnique) {
-                    yield return kv;
-                    keyCollisionGuard.Add(kv.pattern);
+                    distinctKeys.Add((scan, match));
                 }
             }
+            return distinctKeys;
         }
+
         private sealed class NaiveSearch : ISearch<TAlphabet, TMatch>
         {
             private readonly Int32 lengthOfLongestPattern;
-            private readonly List<(List<TAlphabet> pattern, TMatch match)> dictionary;
-
-            public NaiveSearch(IEnumerable<(List<TAlphabet> pattern, TMatch match)> dictionary)
+            private readonly List<(Scan pattern, TMatch match)> dictionary;
+            public NaiveSearch(List<(Scan pattern, TMatch match)> dictionary)
             {
-                this.dictionary = dictionary.ToList();
+                this.dictionary = dictionary;
                 this.lengthOfLongestPattern = this.dictionary.Select(e => e.pattern.Count).Max();
             }
             public IEnumerable<TMatch> Search<TText>(TText input)
                 where TText : IEnumerable<TAlphabet>
             {
-                var searches = new Queue<List<TAlphabet>>(this.lengthOfLongestPattern);
-                searches.Enqueue(new List<TAlphabet>());
+                return Search(input, this.dictionary, this.lengthOfLongestPattern);
+            }
+            private static IEnumerable<TMatch> Search<TText>(TText input, List<(Scan pattern, TMatch match)> dictionary, Int32 max)
+                where TText : IEnumerable<TAlphabet>
+            {
+                var searches = new Queue<Scan>(max);
+                searches.Enqueue(new Scan(max));
                 foreach(var letter in input) {
                     foreach(var search in searches) {
-                        search.Add(letter);
-                        foreach(var (pattern, match) in this.dictionary) {
-                            if(pattern.SequenceEqual(search)) {
+                        search.Add(in letter);
+                        foreach(var (pattern, match) in dictionary) {
+                            if(pattern.Equal(in search)) {
                                 yield return match;
                             }
                         }
                     }
-                    while(searches.Count >= this.lengthOfLongestPattern) {
-                        searches.Dequeue();
+                    Scan next = null;
+                    if(searches.Count >= max) {
+                        next = searches.Dequeue();
+                        next.Clear();
                     }
-                    searches.Enqueue(new List<TAlphabet>());
+                    searches.Enqueue(next ?? new Scan(max));
                 }
+            }
+        }
+
+        private sealed class Scan
+        {
+            private Int32 end;
+            private readonly TAlphabet[] scan;
+            public Int32 Count => this.end;
+            public Scan(Int32 length)
+            {
+                this.end = 0;
+                this.scan = new TAlphabet[length];
+            }
+            public Scan(TAlphabet[] source)
+            {
+                this.end = source.Length;
+                this.scan = source;
+            }
+            public void Add(in TAlphabet character) => this.scan[this.end++] = character;
+            public void Clear() => this.end = 0;
+            public Boolean Equal(in Scan other)
+            {
+                if(this.end != other.end) {
+                    return false;
+                }
+                for(Int32 pos = 0; pos < this.end; ++pos) {
+                    if(!this.scan[pos].Equals(other.scan[pos])) {
+                        return false;
+                    }
+                }
+                return true;
             }
         }
     }
