1deef3247106f7d37222cf701fae8c67c1877e25
Thomas Kägi <kaegit@gmail.com>
2020-12-23 13:32:42 +0100

    Cleanup benchmarking.

--- patternMatching.Benchmark/AhoCorasickBenchmark.cs
+++ patternMatching.Benchmark/AhoCorasickBenchmark.cs
@@ -8,29 +8,35 @@ namespace patternMatching.Benchmark
     public class AhoCorasickBenchmark
     {
 
-        [Params(80, 200)]
+        [Params(60)]
         public Int32 WordsInDictionary { get; set; }
 
-        [Params(200, 4000)]
+        [Params(400)]
         public Int32 TextWordCount { get; set; }
 
-        [Params(12, 48)]
+        [Params(8)]
         public Int32 WordSize { get; set; }
 
         private DataSource source;
-        private ISearch<Char, String> search;
+        private ISearch<Char, String> naive;
+        private ISearch<Char, String> aho;
 
         [GlobalSetup]
         public void Setup()
         {
             this.source = new DataSource(WordSize, WordsInDictionary);
-            this.search = new AhoCorasick<Char, String>() { this.source.Dictionary }.Build();
+            this.aho = new AhoCorasick<Char, String>() { this.source.Dictionary }.Build();
+            this.naive = new Naive.MultiPatternSearch<Char, String> { this.source.Dictionary }.Build();
         }
+
+        [Benchmark(Baseline = true)]
+
+        public List<String> NaiveSearch() => this.naive.Search(this.source.Text(TextWordCount / 50)).ToList();
 
         [Benchmark]
         public ISearch<Char, String> Building() => new AhoCorasick<Char, String>() { source.Dictionary }.Build();
 
         [Benchmark]
-        public List<String> Searching() => this.search.Search(this.source.Text(TextWordCount)).ToList();
+        public List<String> Searching() => this.aho.Search(this.source.Text(TextWordCount)).ToList();
     }
 }
--- patternMatching.Benchmark/DataSource.cs
+++ patternMatching.Benchmark/DataSource.cs
@@ -1,51 +1,49 @@
-
 using System;
 using System.Linq;
-using System.Text;
 using System.Collections.Generic;
 
 namespace patternMatching.Benchmark
 {
     internal sealed class DataSource
     {
-        private readonly String[] textSegment;
-
-        public IEnumerable<String> Dictionary { get; }
-
+        private readonly Random rand;
+        private readonly Int32 wordSize;
+        public List<String> Dictionary { get; }
         public DataSource(Int32 wordSize, Int32 dictionarySize)
         {
-            var rand = new Random(42);
-            Dictionary = BuildWords(rand, wordSize, dictionarySize);
-            this.textSegment = BuildWords(rand, wordSize, 256).ToArray();
+            this.rand = new Random(42);
+            this.wordSize = wordSize;
+            Dictionary = Words(this.rand, (Int32)(wordSize / 1.4), dictionarySize).Select(w => new String(w.ToArray())).ToList();
         }
-
-        public IEnumerable<Char> Text(Int32 wordCount) => InfiniteWords().Take(wordCount).SelectMany(s => s);
-        private IEnumerable<String> InfiniteWords()
+        public IEnumerable<Char> Text(Int32 wordCount) => Text(this.rand, this.wordSize, wordCount, ' ');
+        private IEnumerable<IEnumerable<Char>> Words(Random rand, Int32 wordSize, Int32 wordCount)
+        {
+            var halfWord = wordSize / 2;
+            for(var count = 0; count < wordCount; ++count) {
+                var nextSize = rand.Next(halfWord, wordSize + halfWord);
+                yield return GenerateText(rand).Take(nextSize);
+            }
+        }
+        private IEnumerable<Char> Text(Random rand, Int32 wordSize, Int32 wordCount, Char wordSep)
+        {
+            var halfWord = wordSize / 2;
+            var generator = GenerateText(rand).GetEnumerator();
+            for(var count = 0; count < wordCount; ++count) {
+                var nextSize = rand.Next(halfWord, wordSize + halfWord);
+                for(Int32 size = 0; size < nextSize; ++size) {
+                    yield return generator.Current;
+                    generator.MoveNext();
+                }
+                yield return wordSep;
+            }
+        }
+        private static IEnumerable<Char> GenerateText(Random rand)
         {
             while(true) {
-                foreach(var letter in this.textSegment) {
-                    yield return letter;
+                foreach(var character in rand.Next().ToString()) {
+                    yield return character;
                 }
             }
-        }
-        private static List<String> BuildWords(Random rand, Int32 wordSize, Int32 wordCount)
-        {
-            var words = new List<String>(wordCount);
-            var minWordSize = Math.Max(10, wordSize / 10);
-            while(words.Count < wordCount) {
-                var index = 0;
-                var text = rand.Next().ToString();
-                var builder = new StringBuilder(rand.Next(minWordSize, wordSize));
-                while(builder.Length < builder.Capacity) {
-                    if(index >= text.Length) {
-                        text = rand.Next().ToString();
-                        index = 0;
-                    }
-                    builder.Append(text[index]);
-                }
-                words.Add(builder.ToString());
-            }
-            return words;
         }
     }
 }
